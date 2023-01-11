165c9a0e9563cf21c6cc83c9b3a3d62a5af66d3b
Pricolno <sashadaanton@mail.ru>
2023-01-11 12:47:55 +0300

    add page with CV

--- src/my_portfolio/js/renderContacts.js
+++ src/my_portfolio/js/renderContacts.js
@@ -2,23 +2,33 @@ const contactsContainer = document.querySelector('#icons-container');
 const contactBtn = document.querySelector('a.footer-button');
 
 async function getContacts(){
-    const contactsData = await fetch('./js/contactsData.json');
+    let locList = window.location.pathname.split('/');
+    let pad = "";
+    if (locList.at(-2) == 'pages'){
+        pad = "../";
+    };
 
-    contactsArray = await contactsData.json();
-    // console.log(contactsArray);
+    const contactsData = await fetch(`./${pad}js/contactsData.json`);
     
+    const contactsArray = await contactsData.json();    
     renderContacts(contactsArray);
 };
 
 
 function renderContacts(contactsArray){
+    let locList = window.location.pathname.split('/');
+    let pad = "";
+    if (locList.at(-2) == 'pages'){
+        pad = "../";
+    };
+
     contactsArray.forEach(contactsItem => {
         const contactHTML = `
         <a href="${contactsItem.link}" target="_blank">
-            <img src="./img/icons/${contactsItem.imgSrc}" alt="${contactsItem.title}">
+            <img src="./${pad}img/icons/${contactsItem.imgSrc}" alt="${contactsItem.title}">
         </a>
         `;
-        contactsContainer.insertAdjacentHTML(position='beforeend', contactHTML);
+        contactsContainer.insertAdjacentHTML('beforeend', contactHTML);
     });
 
 };
@@ -32,5 +42,3 @@ function rendercontactBtn(){
 };
 
 rendercontactBtn();
-
-
--- src/my_portfolio/js/renderProjects.js
+++ src/my_portfolio/js/renderProjects.js
@@ -28,3 +28,4 @@ function renderProjects(projectArray){
 };
 
 getProduct();
+
